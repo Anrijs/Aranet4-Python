887cea93f92de048ab3c25b8aaac9554cf767866
Anrijs <anrijs@localhost>
2021-05-11 14:51:42 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("aranet4/README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aranet4",
+    version="1.1.3",
+    description="Aranet4 Python client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Anrijs/Aranet4-Python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
