25cbdfabe7b3654ed7f5e261d066394f882cd735
Anrijs <Anrijs@users.noreply.github.com>
2022-02-19 10:29:02 +0200

    Bump version 2.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("aranet4/README.md", "r") as fh:
 
 setuptools.setup(
     name="aranet4",
-    version="1.1.3",
+    version="2.0.0",
     description="Aranet4 Python client",
     long_description=long_description,
     long_description_content_type="text/markdown",
