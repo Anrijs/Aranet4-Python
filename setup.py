e20dad3302958e8f3e34b499505030de45583c66
Anrijs <Anrijs@users.noreply.github.com>
2022-07-28 10:52:15 +0300

    v2.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("aranet4/README.md", "r") as fh:
 
 setuptools.setup(
     name="aranet4",
-    version="2.0.2",
+    version="2.0.3",
     description="Aranet4 Python client",
     long_description=long_description,
     long_description_content_type="text/markdown",
