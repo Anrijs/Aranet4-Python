06fe7646feb589c284281a958f5077a415d585cb
Anrijs <Anrijs@users.noreply.github.com>
2022-10-07 17:25:48 +0100

    v2.1.0

--- aranet4/__init__.py
+++ aranet4/__init__.py
@@ -1,4 +1,4 @@
 from aranet4.client import Aranet4, Aranet4HistoryDelegate, Aranet4Error, Aranet4Scanner
 
 name = "aranet4"
-__version__ = "1.1.3"
+__version__ = "2.1.0"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aranet4",
-    version="2.0.5",
+    version="2.1.0",
     description="Aranet4 Python client",
     long_description=long_description,
     long_description_content_type="text/markdown",
