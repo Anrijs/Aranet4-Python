517241c40d5f6f71387340fb2da0b1fa92816070
Matthew <5273302+Matthew1471@users.noreply.github.com>
2024-09-16 04:53:12 +0100

    Improve scanner_simple.py (#45)
    * Improve scanner_simple.py
    
    - `BLEDevice.rssi` is deprecated since version 0.19.0 (see https://bleak.readthedocs.io/en/latest/api/index.html#bleak.backends.device.BLEDevice.rssi) switched to using the advertisement rssi (see https://bleak.readthedocs.io/en/latest/api/scanner.html#bleak.BleakScanner.discover).
    - Comment states scan runs for 10 seconds but it actually runs for 5, corrected to run for 10 as it is not detecting Aranet4 in my environment at only 5 seconds.
    - Battery has a "&" symbol after it instead of a percentage symbol (typo)?.
    
    * Improve Console Consistency
    
    - Fixed formatting of scanner_simple.py to be more consistent with the client advertising printout.
    - Added unit to client.py age.
    - Fixed another typo in client.py.

--- examples/scanner/scanner_simple.py
+++ examples/scanner/scanner_simple.py
@@ -5,45 +5,47 @@ scanned_devices = {}
 
 def on_scan(advertisement):
     if advertisement.device.address not in scanned_devices:
-        print(f"Found new device:  {advertisement.device.name}")
+        print(f"Found device:  {advertisement.device.name}")
 
     scanned_devices[advertisement.device.address] = advertisement
 
 def print_advertisement(advertisement):
     print("=======================================")
-    print(f"  Name:              {advertisement.device.name}")
-    print(f"  Address:           {advertisement.device.address}")
+    print(f"  Name:         {advertisement.device.name}")
+    print(f"  Address:      {advertisement.device.address}")
 
     if advertisement.manufacturer_data:
         mf_data = advertisement.manufacturer_data
-        print(f"  Version:           {mf_data.version}")
-        print(f"  Integrations:      {mf_data.integrations}")
-        # print(f"  Disconnected:      {mf_data.disconnected}")
-        # print(f"  Calibration state: {mf_data.calibration_state.name}")
-        # print(f"  DFU Active:        {mf_data.dfu_active:}")
+        print(f"  Version:      {mf_data.version}")
+        print(f"  Integrations: {mf_data.integrations}")
+        # print(f"  Disconnected: {mf_data.disconnected}")
+        # print(f"  Calibration:  {mf_data.calibration_state.name}")
+        # print(f"  DFU Active:   {mf_data.dfu_active:}")
 
-    print(f"  RSSI:              {advertisement.device.rssi} dBm")
+    print(f"  RSSI:         {advertisement.rssi} dBm")
 
     if advertisement.readings:
         readings = advertisement.readings
-        print("-------------------------------------")
-        print(f"  CO2:           {readings.co2} pm")
-        print(f"  Temperature:   {readings.temperature:.01f} \u00b0C")
-        print(f"  Humidity:      {readings.humidity} %")
-        print(f"  Pressure:      {readings.pressure:.01f} hPa")
-        print(f"  Battery:       {readings.battery} &")
-        print(f"  Status disp.:  {readings.status.name}")
-        print(f"  Ago:           {readings.ago} s")
+        print("---------------------------------------")
+        print(f"  CO2:            {readings.co2} pm")
+        print(f"  Temperature:    {readings.temperature:.01f} \u00b0C")
+        print(f"  Humidity:       {readings.humidity} %")
+        print(f"  Pressure:       {readings.pressure:.01f} hPa")
+        print(f"  Battery:        {readings.battery} %")
+        print(f"  Status Display: {readings.status.name}")
+        print(f"  Age:            {readings.ago}/{readings.interval} s")
     print()
 
 
 def main(argv):
     # Scan for 10 seconds, then print results
-    print("Scanning Aranet4 devices...")
-    aranet4.client.find_nearby(on_scan, 5)
-    print(f"\nFound {len(scanned_devices)} devices:\n")
+    print("Looking for Aranet devices...")
+    print()
+    aranet4.client.find_nearby(on_scan, 10)
+    print(f"\nFound {len(scanned_devices)} devices:")
 
     for addr in scanned_devices:
+        print()
         advertisement = scanned_devices[addr]
         print_advertisement(advertisement)
 
